920d569f0fefa79537c09a98133e09459c5d5221
PatrickStephan25 <pjs0031@mix.wvu.edu>
2022-02-03 08:05:48 -0500

    Lab 2 2 new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmailsComponent } from './content/emails.component';
+import { NavBarComponent } from './header/navbar.component';
 import { SideNavBarComponent } from './header/sidenavbar.component';
 import { TopNavBarComponent } from './header/topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
     SideNavBarComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    EmailsComponent
   ],
   imports: [
     BrowserModule
