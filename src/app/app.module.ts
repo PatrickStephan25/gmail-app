992c1e3a11c90338f7bb6a2eed7276dadceea6c0
PatrickStephan25 <pjs0031@mix.wvu.edu>
2022-03-23 14:08:52 -0400

    Lab assignment 8 2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,11 @@ import { SentEmailNavBarComponent } from './content/sentemailnavbar.component';
 import { MoreEmailsComponent } from './content/moreemails.component';
 import {HttpClientModule} from '@angular/common/http'
 import { UserInfoComponent } from './user-info/user-info.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
@@ -42,7 +47,9 @@ import { UserInfoComponent } from './user-info/user-info.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/emails.service.ts
+++ src/app/content/emails.service.ts
@@ -1,22 +1,21 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { EmailModel } from "./email.model";
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable(
     {providedIn:'root'}
 )
 export class EmailsService{
-    private baseUrl:string="https://app-9a2c8-default-rtdb.firebaseio.com/";
-    private emailsEndPoint:string="emails.json";
+    
 
-    constructor(private http:HttpClient){
+    constructor(private db:AngularFireDatabase) {
 
     }
 
     getEmails(){
-        return this.http.get<EmailModel[]>(this.baseUrl+this.emailsEndPoint);
+        return this.db.list<EmailModel>("emails").valueChanges();
     }
     getEmail(index:number){
-        return this.http.get<EmailModel[]>(this.baseUrl+ 'emails' +'/'+index+'.json');
+        
     }
 }
